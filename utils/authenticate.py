d9bee86ffb0f220c1b199f0f4f15a6bd1f3fe8ce
RishikaPRO <rishika.murugesh@srmtech.com>
2026-06-24 16:48:00 +0530

    auth

--- utils/authenticate.py
+++ utils/authenticate.py
@@ -1,21 +1,34 @@
+from pathlib import Path
 import pandas as pd
 import streamlit as st
-def authenticate(username,password):
+ 
+def authenticate(username, password):
     try:
-        df=pd.read_excel("Login.xlsx")
+        BASE_DIR = Path(__file__).resolve().parent.parent
+        file_path = BASE_DIR / "Login.xlsx"
+ 
+        st.write("Reading:", file_path)
+ 
+        df = pd.read_excel(file_path)
         st.write(df)
-
-        print(df.shape)
-        df.columns=df.columns.str.strip().str.lower()
-        user=df[(df["username"].astype(str).str.strip()==str(username)) & (df["password"].astype(str).str.strip()==str(password))]
+ 
+        df.columns = df.columns.str.strip().str.lower()
+ 
+        user = df[
+            (df["username"].astype(str).str.strip() == str(username).strip()) &
+            (df["password"].astype(str).str.strip() == str(password).strip())
+        ]
+ 
         if not user.empty:
-            row=user.iloc[0]
+            row = user.iloc[0]
             return {
-                "role":row["role"],
-                "team":row["team"]}
-        else:
-            return None
-    except Exception as e:
-        print(f"Error during authentication: {e}")
+                "role": row["role"],
+                "team": row["team"]
+            }
+ 
         return None
-    
+ 
+    except Exception as e:
+        st.error(f"Authentication error: {e}")
+        return None
+ 
